0f6f04ff1f1a7b8f5826bffc0e0b5eac82d38cfc
Tekehiko69 <78441668+Tekehiko69@users.noreply.github.com>
2021-05-09 00:49:35 +0300

    Сделал подельку, сделал миграции и создал суперюзера

--- website/news/models.py
+++ website/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50)
+    anons = models.CharField('Анонс', max_length=200)
+    full_text = models.TextField('Статья')
+    date = models.DateField('Дата публикации')
+
+    def __str__(self):
+        return self.title
