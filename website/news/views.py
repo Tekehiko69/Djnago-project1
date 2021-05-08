e1872beb665f7b21b308d56dc4f5bd5ddeeba10f
Tekehiko69 <78441668+Tekehiko69@users.noreply.github.com>
2021-05-09 02:59:34 +0300

    Создал класс для обработки шаблонов

--- website/news/views.py
+++ website/news/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-# Create your views here.
+from django.views.generic import DetailView
+
+
 def news(request):
     news = Articles.objects.order_by('date')
     return render(request, 'news/news_home.html', {'news': news})
+
+class NewDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
 
 def create(request):
     error = ''
