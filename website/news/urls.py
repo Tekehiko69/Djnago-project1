4f7fd33e304a44f2449936f326be9420f994f4f6
Tekehiko69 <78441668+Tekehiko69@users.noreply.github.com>
2021-05-09 03:02:38 +0300

    Отслеживание url адреса

--- website/news/urls.py
+++ website/news/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.news, name='news-page'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewDetailView.as_view(), name='details-page')
 
 ]
